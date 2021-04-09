ff74fadc00d203de2ac06c2c43f70987a95aeaaf
kirilllebedev <Lebedev.a.l@mail.ru>
2021-04-09 23:50:32 +0300

    Add category persist

--- simple_jsf_app/src/main/java/ru/gee/persist/Category.java
+++ simple_jsf_app/src/main/java/ru/gee/persist/Category.java
@@ -1,8 +1,20 @@
 package ru.gee.persist;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+@NamedQueries({
+        @NamedQuery(name = "deleteCategoryById", query = "delete from Category c where c.id = :id"),
+        @NamedQuery(name = "findAllCategory", query = "from Category c"),
+        @NamedQuery(name = "countCategory", query = "select count(c) from Category c")
+})
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
     public Category() {
